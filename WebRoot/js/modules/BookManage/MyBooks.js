d81b7a8a6932c8b776efa6c0d05411890264ab73
PureDark <bruce.lee.liang@gmail.com>
2016-02-29 15:38:07 +0800

    完成从藏书页面打开书籍详情

--- WebRoot/js/modules/BookManage/MyBooks.js
+++ WebRoot/js/modules/BookManage/MyBooks.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 	$('.parallax').parallax();
 	$( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
+	
+	
+	$(document).delegate(".BookCard", "click", function(e){
+		$("#bookDetailModal").openModal();
+	});
 });

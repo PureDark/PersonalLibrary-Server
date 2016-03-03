6c520c6af1ca45c74ce75d0510bd1735726c28be
xyf0921 <974708644@qq.com>
2016-03-03 11:53:57 +0800

    代码规范化

--- WebRoot/js/modules/FriendsManage/FriendsManage.js
+++ WebRoot/js/modules/FriendsManage/FriendsManage.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.modal-trigger').leanModal();
     $('#requestWrapper').pushpin({ top: $('#requestWrapper').offset().top });
     $('ul.tabs').tabs();
-  });
+ });
+ 
+ function accept(rid){
+	 $(".tabs li:eq(1) a").click();
+ }

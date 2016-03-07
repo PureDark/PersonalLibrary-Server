e64c25a716114d2b3118e7b90d8998b5dcbc1bf4
PureDark <bruce.lee.liang@gmail.com>
2016-03-07 21:00:26 +0800

    封装好对服务器API的JS调用方法

---
+++ WebRoot/js/APIHelper.js
@@ -0,0 +1,491 @@
+/**
+ * Created by PureDark on 2016/03/07.
+ * 用于简化服务端API调用的帮助类，对所有API调用进行了封装
+ */
+ 
+	var PLServerAPI = {
+		login: function(cellphone, password, callback){
+			var params = {
+						module: "user",
+						action: "login",
+						cellphone: cellphone,
+						password: password
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							callback.onSuccess(data);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		logout: function(callback){
+			var params = {
+						module: "user",
+						action: "logout"
+			};
+			postNoReturnData(params, {
+						onSuccess : function(){
+							callback.onSuccess(null);
+                        	window.location.href = "./login.html";
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		verifyCellphoneUnused: function(cellphone, callback){
+			var params = {
+						module: "user",
+						action: "verifyCellphoneUnused",
+						cellphone: cellphone
+			};
+			postNoReturnData(params, callback);
+		},
+		sendCaptcha: function(cellphone, callback){
+			var params = {
+						module: "user",
+						action: "sendCaptcha",
+						cellphone: cellphone
+			};
+			postReturnJsonElement(params, callback);
+		},
+		register: function(cellphone, password, captcha, callback){
+			var params = {
+						module: "user",
+						action: "register",
+						cellphone: cellphone,
+						password: password,
+						captcha: captcha
+			};
+			postNoReturnData(params, callback);
+		},
+		changePassword: function(oldpass, newpass, callback){
+			var params = {
+						module: "user",
+						action: "changePassword",
+						oldpass: oldpass,
+						newpass: newpass
+			};
+			postNoReturnData(params, callback);
+		},
+		resetPassword: function(cellphone, password, captcha, callback){
+			var params = {
+						module: "user",
+						action: "resetPassword",
+						cellphone: cellphone,
+						password: password,
+						captcha: captcha
+			};
+			postNoReturnData(params, callback);
+		},
+		modifyUserInfo: function(nickname, sex, signature, birthday, callback){
+			var params = {
+						module: "user",
+						action: "modifyUserInfo",
+						nickname: nickname,
+						sex: sex,
+						signature: signature,
+						birthday: birthday
+			};
+			postNoReturnData(params, callback);
+		},
+		getUserInfo: function(callback){
+			var params = {
+						module: "user",
+						action: "getUserInfo"
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							
+							var userInfo = 
+								UserInfo.newInstance(data.uid, data.sex, data.nickname, data.signature, data.birthday);
+							callback.onSuccess(userInfo);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getUidByCellphone: function(cellphone, callback){
+			var params = {
+						module: "user",
+						action: "getUidByCellphone",
+						cellphone: cellphone
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							callback.onSuccess(data.uid);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		addBook: function(isbn13, cover, title, author, summary, callback){
+			var params = {
+						module: "library",
+						action: "addBook",
+						isbn13: isbn13,
+						cover: cover,
+						title: title,
+						author: author,
+						summary: summary
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var bid = data.bid;
+							var book = BookListItem.newInstance(bid, isbn13, cover, title, author, summary);
+							callback.onSuccess(book);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		deleteBook: function(bid, callback){
+			var params = {
+						module: "library",
+						action: "deleteBook",
+						bid: bid
+			};
+			postNoReturnData(params, callback);
+		},
+		addTag: function(bid, tag, callback){
+			var params = {
+						module: "library",
+						action: "addTag",
+						bid: bid,
+						tag: tag
+			};
+			postNoReturnData(params, callback);
+		},
+		deleteTag: function(bid, tid, callback){
+			var params = {
+						module: "library",
+						action: "deleteTag",
+						bid: bid,
+						tid: tid
+			};
+			postNoReturnData(params, callback);
+		},
+		getTagList: function(callback){
+			var params = {
+						module: "library",
+						action: "getTagList"
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var tags = [];
+							$.each(data, function(i, tag){
+								tags[i] = Tag.newInstance(tag.tid, tag.title);
+							});
+							callback.onSuccess(tags);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getBookList: function(uid, tids, keyword, callback){
+			var params = {
+						module: "library",
+						action: "getBookList",
+						uid: uid,
+						tids: tids,
+						keyword: keyword
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var books = [];
+							$.each(data, function(i, book){
+								books[i] = BookListItem.newInstance(book.bid, book.isbn13, book.cover, book.title, 
+																	book.author, book.summary);
+							});
+							callback.onSuccess(books);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		addBorrowRecord: function(bid, loan_uid, callback){
+			var params = {
+						module: "borrow",
+						action: "addBorrowRecord",
+						bid: bid,
+						loan_uid: loan_uid
+			};
+			postNoReturnData(params, callback);
+		},
+		acceptBorrowRecord: function(brid, accept, callback){
+			var params = {
+						module: "borrow",
+						action: "acceptBorrowRecord",
+						brid: brid,
+						accept: (accept)?1:0
+			};
+			postNoReturnData(params, callback);
+		},
+		setBookReturned: function(brid, callback){
+			var params = {
+						module: "borrow",
+						action: "setBookReturned",
+						brid: brid
+			};
+			postNoReturnData(params, callback);
+		},
+		getBorrowedBookRecordList: function(status, callback){
+			var params = {
+						module: "borrow",
+						action: "getBorrowedBookRecordList",
+						status: status
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var borrowRecords = [];
+							$.each(data, function(i, br){
+								borrowRecords[i] = BorrowRecord.newInstance(br.brid, br.loan_uid, br.borrow_uid, 
+									br.nickname, br.book_id , br.book_name, br.borrow_time, br.return_time, br.status);
+							});
+							callback.onSuccess(borrowRecords);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getLoanedBookRecordList: function(status, callback){
+			var params = {
+						module: "borrow",
+						action: "getLoanedBookRecordList",
+						status: status
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var borrowRecords = [];
+							$.each(data, function(i, br){
+								borrowRecords[i] = BorrowRecord.newInstance(br.brid, br.loan_uid, br.borrow_uid, 
+									br.nickname, br.book_id , br.book_name, br.borrow_time, br.return_time, br.status);
+							});
+							callback.onSuccess(borrowRecords);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		searchUser: function(keyword, callback){
+			var params = {
+						module: "social",
+						action: "searchUser",
+						keyword: keyword
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var friends = [];
+							$.each(data, function(i, friend){
+								friends[i] = Friend.newInstance(friend.uid, friend.sex, friend.nickname, 
+													friend.signature, friend.birthday, friend.isFriend);
+							});
+							callback.onSuccess(friends);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		addRequest: function(fid, callback){
+			var params = {
+						module: "social",
+						action: "addRequest",
+						fid: fid
+			};
+			postNoReturnData(params, callback);
+		},
+		responseRequest: function(rid, accept, callback){
+			var params = {
+						module: "social",
+						action: "responseRequest",
+						rid: rid,
+						accept: (accept)?1:0
+			};
+			postNoReturnData(params, callback);
+		},
+		deleteFriends: function(fid, callback){
+			var params = {
+						module: "social",
+						action: "deleteFriends",
+						fid: fid
+			};
+			postNoReturnData(params, callback);
+		},
+		getRequestList: function(page, callback){
+			var params = {
+						module: "social",
+						action: "getRequestList",
+						page: page
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var requests = [];
+							$.each(data, function(i, req){
+								requests[i] = Request.newInstance(req.rid, req.uid, req.nickname, req.status);
+							});
+							callback.onSuccess(requests);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getFriendList: function(page, callback){
+			var params = {
+						module: "social",
+						action: "getFriendList",
+						page: page
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var friends = [];
+							$.each(data, function(i, friend){
+								friends[i] = Friend.newInstance(friend.uid, friend.sex, friend.nickname, 
+													friend.signature, friend.birthday, friend.isFriend);
+							});
+							callback.onSuccess(friends);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		addBookMark: function(bid, title, content, callback){
+			var params = {
+						module: "social",
+						action: "addBookMark",
+						bid: bid,
+						title: title,
+						content: content
+			};
+			postNoReturnData(params, callback);
+		},
+		deleteBookMark: function(mid, callback){
+			var params = {
+						module: "social",
+						action: "deleteBookMark",
+						mid: mid
+			};
+			postNoReturnData(params, callback);
+		},
+		getBookMarkList: function(bid, uid, callback){
+			var params = {
+						module: "social",
+						action: "getBookMarkList",
+						bid: bid,
+						uid: uid
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var bookMarks = [];
+							$.each(data, function(i, bm){
+								bookMarks[i] = BookMark.newInstance(bm.mid, bm.title, bm.summary, bm.content, bm.time, 
+									bm.bid, bm.isbn13, bm.book_title, bm.book_cover, bm.uid, bm.nickname, bm.signature);
+							});
+							callback.onSuccess(bookMarks);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getRecentBookMarks: function(uid, callback){
+			var params = {
+						module: "social",
+						action: "getRecentBookMarks",
+						uid: uid
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var bookMarks = [];
+							$.each(data, function(i, bm){
+								bookMarks[i] = BookMark.newInstance(bm.mid, bm.title, bm.summary, bm.content, bm.time, 
+									bm.bid, bm.isbn13, bm.book_title, bm.book_cover, bm.uid, bm.nickname, bm.signature);
+							});
+							callback.onSuccess(bookMarks);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+		getBookMarkDetails: function(mid, callback){
+			var params = {
+						module: "social",
+						action: "getBookMarkDetails",
+						mid: mid
+			};
+			postReturnJsonElement(params, {
+						onSuccess : function(data){
+							var bm = data;
+							var bookMark = BookMark.newInstance(bm.mid, bm.title, bm.summary, bm.content, bm.time, 
+									bm.bid, bm.isbn13, bm.book_title, bm.book_cover, bm.uid, bm.nickname, bm.signature);
+							callback.onSuccess(bookMark);
+						},
+					  	onFailure : function(apiError){
+							callback.onFailure(apiError);
+						}
+			});
+		},
+
+	};
+	
+	function postNoReturnData(params, callback){
+		var baseUrl = "./servlet/manager";
+		$.ajax({
+			 type: "POST",
+			 url: baseUrl,
+			 data: params,
+			 dataType: "json",
+			 success: function(result){
+				 if(result.status){
+					 callback.onSuccess();
+				 }else{
+					var errorCode = result.errorCode;
+                    if (errorCode === 1002){
+                        window.location.href = "./login.html";
+					}else{
+						callback.onFailure(ApiError.newInstance(errorCode));
+					}
+				 }
+			},
+			error:function(xhr){
+				 callback.onFailure(ApiError.newInstance(1009));
+			}
+		});
+	}
+	
+	function postReturnJsonElement(params, callback){
+		var baseUrl = "./servlet/manager";
+		$.ajax({
+			 type: "POST",
+			 url: baseUrl,
+			 data: params,
+			 dataType: "json",
+			 success: function(result){
+				 if(result.status){
+					 callback.onSuccess(result.data);
+				 }else{
+					var errorCode = result.errorCode;
+                    if (errorCode === 1002){
+                        window.location.href = "./login.html";
+					}else{
+						callback.onFailure(ApiError.newInstance(errorCode));
+					}
+				 }
+			},
+			error:function(xhr){
+				 callback.onFailure(ApiError.newInstance(1009));
+			}
+		});
+	}
---
+++ WebRoot/js/beans.js
@@ -0,0 +1,134 @@
+/**
+ * Created by PureDark on 2016/03/07.
+ * 用到的所有数据结构
+ */
+
+	var ApiError = {
+		newInstance: function(errorCode){
+			var apiError = {};
+			apiError.errorCode = errorCode;
+
+			var errorString;
+			switch(errorCode){
+				case 1000:errorString="未知错误";break;
+				case 1001:errorString="参数不全";break;
+				case 1002:errorString="尚未登录";break;
+				case 1003:errorString="模块不存在";break;
+				case 1004:errorString="没有权限";break;
+				case 1005:errorString="json解析错误";break;
+				case 1006:errorString="无此接口";break;
+				case 1009:errorString="网络错误，请重试";break;
+				case 1010:errorString="数据库错误";break;
+				case 1011:errorString="密码错误";break;
+				case 1012:errorString="用户不存在";break;
+				case 1021:errorString="用户无此书";break;
+				case 1022:errorString="书籍不存在";break;
+				case 1023:errorString="Tag不存在";break;
+				case 1032:errorString="手机号已被使用";break;
+				case 1051:errorString="书评不属于该用户";break;
+				case 1061:errorString="对方不拥有该书籍";break;
+				case 1062:errorString="书籍已借出";break;
+				case 1063:errorString="已经发送过借该书的请求";break;
+				case 1064:errorString="无效的借书请求";break;
+				default:errorString="未定义的错误码";break;
+			}
+			apiError.errorString = errorString;
+			apiError.getErrorCode = function(){ return apiError.errorCode };
+			apiError.getErrorMessage = function(){ return apiError.errorString };
+			return apiError;
+		}
+	};
+	
+	var UserInfo = {
+		newInstance: function(uid, sex, nickname, signature, birthday){
+			var userInfo = {};
+			userInfo.uid = uid;
+			userInfo.sex = sex;
+			userInfo.nickname = nickname;
+			userInfo.signature = signature;
+			userInfo.birthday = birthday;
+			return userInfo;
+		}
+	};
+	
+	var BookListItem = {
+		newInstance: function(bid, isbn13, cover, title, author, summary){
+			var bookListItem = {};
+			bookListItem.bid = bid;
+			bookListItem.isbn13 = isbn13;
+			bookListItem.cover = cover;
+			bookListItem.title = title;
+			bookListItem.author = author;
+			bookListItem.summary = summary;
+			return bookListItem;
+		}
+	};
+	
+	var Tag = {
+		newInstance: function(tid, title){
+			var tag = {};
+			tag.tid = tid;
+			tag.title = title;
+			return tag;
+		}
+	};
+	
+	var BorrowRecord = {
+		newInstance: function(brid, loan_uid, borrow_uid, nickname, book_id , book_name, borrow_time, return_time, status){
+			var borrowRecord = {};
+			borrowRecord.brid = brid;
+			borrowRecord.loan_uid = loan_uid;
+			borrowRecord.borrow_uid = borrow_uid;
+			borrowRecord.nickname = nickname;
+			borrowRecord.book_id = book_id;
+			borrowRecord.book_name = book_name;
+			borrowRecord.borrow_time = borrow_time;
+			borrowRecord.return_time = return_time;
+			borrowRecord.status = status;
+			return borrowRecord;
+		}
+	};
+	
+	var Friend = {
+		newInstance: function(uid, sex, nickname, signature, birthday, isFriend){
+			var friend = {};
+			friend.uid = uid;
+			friend.sex = sex;
+			friend.nickname = nickname;
+			friend.signature = signature;
+			friend.birthday = birthday;
+			friend.isFriend = isFriend;
+			return friend;
+		}
+	};
+	
+	var Request = {
+		newInstance: function(rid, uid, nickname, status){
+			var request = {};
+			request.rid = rid;
+			request.uid = uid;
+			request.nickname = nickname;
+			request.status = status;
+			return request;
+		}
+	};
+	
+	var BookMark = {
+		newInstance: function(mid, title, summary, content, time, bid, isbn13, book_title, book_cover, 
+							uid, nickname, signature){
+			var bookMark = {};
+			bookMark.mid = mid;
+			bookMark.title = title;
+			bookMark.summary = summary;
+			bookMark.content = content;
+			bookMark.time = time;
+			bookMark.bid = bid;
+			bookMark.isbn13 = isbn13;
+			bookMark.book_title = book_title;
+			bookMark.book_cover = book_cover;
+			bookMark.uid = uid;
+			bookMark.nickname = nickname;
+			bookMark.signature = signature;
+			return bookMark;
+		}
+	};

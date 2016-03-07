659d6972cf2e134d53a8450e3db634055e79df6b
xyf0921 <974708644@qq.com>
2016-03-07 16:19:54 +0800

    书友藏书模块

---
+++ WebRoot/js/modules/Friend/FriendDetail.js
@@ -0,0 +1,30 @@
+;(function(window, document, $){
+
+  $(document).ready(function(){
+
+	// Make some selections.
+	var $window       = $(window);
+	var $imgWrapper   = $('.image-wrapper');
+	var $imgs         = $imgWrapper.find("img");
+
+	$imgs.on('ab-color-found', function(e, data){
+	  $(this).parents('.image-wrapper')
+			 .attr('data-color', data.color);
+
+	  $(this).css({ 
+		border: "1px solid " + data.palette[0].replace(')', ",0.25)").replace('rgb', "rgba") 
+	  });
+
+	  $(this).parents('.image-wrapper')
+			 .css({ background: data.color })
+	});
+
+	// Run the A.B. plugin.
+	$.adaptiveBackground.run({ parent: "1" });
+  })
+
+})(window, document, jQuery)
+
+function getBook(bid){ 
+	  alert(bid);
+}

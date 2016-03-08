b8fca8e7b316d54056626353a1d59af2161a1c63
xyf0921 <974708644@qq.com>
2016-03-08 20:03:50 +0800

    friend js

--- WebRoot/js/modules/Friend/FriendDetail.js
+++ WebRoot/js/modules/Friend/FriendDetail.js
@@ -1,3 +1,4 @@
+var Info;
 ;(function(window, document, $){
 
   $(document).ready(function(){
@@ -25,6 +26,38 @@
 
 })(window, document, jQuery)
 
-function getBook(bid){ 
-	  alert(bid);
-}
+
+PLServerAPI.getUserInfo({
+	OnSuccess: function(userInfo){
+		Info = userInfo;
+		$("#FriendInfo").append(
+		'<img src="http://115.28.135.76/images/users/avatars/'+userInfo.uid+'.png" alt="" class="circle" style="height:130px; margin-top:18px" data-adaptive-background="1">'+
+		'<ul style="margin-left: 40px ">'+
+			'<li class="flow-text" style="width: 150px">'+
+				'<h5 class="truncate" style="margin-left:10px ; margin-top:30px">'+userInfo.nickname+'</h5>'+
+			'</li>'+
+			'<li class="chip booktab" style="margin-top: 15px"><span class="truncate" style="width: 300px">'+userInfo.signature+'</span>'	+
+			'</li>'+
+		'</ul>'
+		);
+	},
+	onFailure: function(apiError){
+		var ErrorCode = apiError.getErrorCode();
+		var ErrorMessage = apiError.getErrorMessage();
+		alert(ErrorCode+":"+ErrorMessage);
+	}
+});
+/*PLServerAPI.getBookList(Info.uid,null,null,{
+	OnSuccess :function(books){
+		$.each(books, function(i,book){
+			$("#FriendBooks").append(
+				'<li class="card hoverable BookCover BookShow">'+
+                    '<img src='+book.cover+' class="BookCover">'+
+                '</li>'
+			);
+		})
+	},
+	onFailure:function(apiError){
+		
+	}
+});*/
--- WebRoot/js/modules/Friend/FriendManage.js
+++ WebRoot/js/modules/Friend/FriendManage.js
@@ -1,3 +1,4 @@
+var currPage = 1;
 $(document).ready(function(){
 	$('.parallax img').attr("src","images/background10.jpg");
     $('.modal-trigger').leanModal();
@@ -5,6 +6,12 @@ $(document).ready(function(){
     $('#requestWrapper').pushpin({ top: $('#MyFriendsCard').offset().top });
     $('ul.tabs').tabs();
 	
+	$(document).undelegate(".GetIn", "click");
+	$(document).delegate(".GetIn", "click", function(e){
+		$("#FriendDetailModal").openModal();
+	});
+	
+	getFriendList(1);
 
  });
  
@@ -12,25 +19,43 @@ $(document).ready(function(){
 	 $(".tabs li:eq(1) a").click();
  }
 
-PLServerAPI.searchUser($("#FriendName").val(), {
-		onSuccess: function(friends){
-			
-		},
-		onFailure: function(apiError){
+
+function getFriendList(page){
+	$(".pagination").empty();
+	$("#FriendList").empty();
+	if(page<=3){
+		for(var i=1;i<6;i++){
+			$(".pagination").append('<li class="waves-effect"><a onClick="getFriendList('+i+')">'+i+'</a></li>');
 		}
+		$(".pagination li:eq("+(page-1)+")").addClass("active");
+	}else{
+		for(var i=page-2;i<page+3;i++){
+			$(".pagination").append('<li class="waves-effect"><a onClick="getFriendList('+i+')">'+i+'</a></li>');
+		}
+		$(".pagination li:eq(2)").addClass("active");
+	}
+	currPage = page;
+	PLServerAPI.getFriendList(currPage,{
+			onSuccess: function(friends){
+				$.each(friends, function(i, friend){
+						
+						$("#FriendList").append(
+							  '<li class="collection-item avatar">'+
+								  '<img src="http://115.28.135.76/images/users/avatars/'+friend.uid+'.png" alt="" class="circle">'+
+								  '<span class="title">'+friend.nickname+'</span>'+
+								  '<p class="Blog">'+friend.signature+
+								  '</p>'+
+								  '<a class="secondary-content waves-effect waves-light btn GetIn">进入TA的主页</a>'+
+							   ' </li>'	);		
+				});
+			},
+			onFailure: function(apiError){
+			var ErrorCode = apiError.getErrorCode();
+			var ErrorMessage = apiError.getErrorMessage();
+			alert(ErrorCode+":"+ErrorMessage);
+			}
 	});
-	
-PLServerAPI.getFriendList("1",{
-		onSuccess: function(friends){
-			'<li class="collection-item avatar">'+
-                              '<img src="'+ +'" alt="" class="circle">'+
-                              '<span class="title">Title</span>'+
-                              '<p class="Blog">First Line '+
-                                ' Second Line'+
-                             ' </p>'+
-                              '<a href="#!" class="secondary-content waves-effect waves-light btn">进入TA的主页</a>'+
-                           ' </li>'
-		},
-		onFailure: function(apiError){
-		}
-});
+}
+
+
+

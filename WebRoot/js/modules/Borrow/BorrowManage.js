27c701d60062ee8a4ddeba94b6fb330d49ecff46
perfectshils <1052165618@qq.com>
2016-03-09 09:53:11 +0800

    借书记录95%完成

--- WebRoot/js/modules/Borrow/BorrowManage.js
+++ WebRoot/js/modules/Borrow/BorrowManage.js
@@ -1,5 +1,4 @@
 $(document).ready(function(e) {
-	$("#Borrow").empty();
 	$('.parallax img').attr("src","images/background2.jpg");
 		
 	$("#BorrowIn .collapsible-header").click(function(){
@@ -9,117 +8,129 @@ $(document).ready(function(e) {
 	$("#BorrowOut .collapsible-header").click(function(){
 		$("#TabBorrowOut").children("a").click();
 	});
-	PLServerAPI.getBorrowedBookRecordList(0, {
-		onSuccess:function(borrowRecords){
-			borrowtime=borrowRecords.borrow_time;
-			PLServerAPI.getloanedBookRecordList(0, {
-				onSuccess:function(loanedRecords){
-					var records = borrowRecords.concat(loanedRecords);
-					$.each(records, function(i, record){
-						var date = record.borrow_time;
-						date = date.substring(0,19);    
-						date = date.replace(/-/g,'/'); 
-						var timestamp = new Date(date).getTime();
-						record.timestamp = timestamp;
-					});
-					$.each(records, function(i, record){
-						record.timestamp
-					});
-				},
-				onFailure: function(apiError){
-					Materialize.toast(apiError.getErrorMessage(), 4000)
-				}
-			
-			});
-			
-		},
-		onFailure: function(apiError){
-			Materialize.toast(apiError.getErrorMessage(), 4000)
-		}
 	
-	});
+	getRecords();
 	
-	
-	//var borrowtime = borrowRecord.borrow_time;
-	//var loanedtime=loanedRecord._time
-	//var stime= time.split(" ");
-	
-	PLServerAPI.getBorrowedBookRecordList(0, {
-		onSuccess: function(borrowRecords){
-			var sstatus;
-			$.each(borrowRecords, function(i, borrowRecord){
-				if(borrowRecord.status==1){
-					sstatus="等待处理";
-				}else if(borrowRecord.status==2){
-					sstatus="已同意";
-				}else if(borrowRecord.status==3){
-					sstatus="被拒绝";
-				}else if(borrowRecord.staus==4){
-					sstatus="已归还";
-				}else{
-					sstatus="";
-				}
+});
+
+	function getRecords(){
+		PLServerAPI.getBorrowedBookRecordList(0, {
+			onSuccess:function(borrowRecords){
+				borrowtime=borrowRecords.borrow_time;
+				PLServerAPI.getLoanedBookRecordList(0, {
+					onSuccess:function(loanedRecords){
+						$("#Borrow").empty();
+						var records = borrowRecords.concat(loanedRecords);
+						$.each(records, function(i, record){
+							var date = record.borrow_time;
+							date = date.substring(0,19);    
+							date = date.replace(/-/g,'/'); 
+							var timestamp = new Date(date).getTime();
+							record.timestamp = timestamp;
+						});
+						for(var i=0;i<records.length-1;i++){
+							for(var j=0;j<records.length-i-1;j++){
+								if(records[j].timestamp<records[j+1].timestamp){
+									var swap=records[j];
+									records[j]=records[j+1];
+									records[j+1]=swap;
+								 }
+							 }
+						 }
+						$.each(records, function(i, record){
+							if(record.isBorrowed)
+								insertBorrowRecord(record);
+							else
+								insertLoanRecord(record);
+						});
+					},
+					onFailure: function(apiError){
+						Materialize.toast(apiError.getErrorMessage(), 4000)
+					}
 				
+				});
 				
-				$("#Borrow").append(
-					'<li id="BorrowIn1" class="row">'+
-						'<time class="cbp_tmtime col m2 l3 offset-l6 hide-on-small-only" datetime='+borrowRecord.borrow_time+'>'+
-							'<span>'+stime[0]+'</span>'+
-							'<span>'+stime[1]+'</span>'+
-						'</time>'+
-						'<div class="cbp_tmicon cbp_tmicon-phone hide-on-med-and-down"></div>'+
-							'<div class="col s12 m10 l5 offset-l0">'+
-								'<div class="card white darken-1 hoverable borrowcard">'+
-									'<div class="BookDetail">'+
-										'<div class="card-content black-text" style="height: 100%">'+
-											'<div style="overflow: auto;">'+
-												'<div class="chips">'+
-													'<img src="http://115.28.135.76/images/users/avatars/'+borrowRecord.loan_uid+'.png" alt="Contact Person">'+
-													borrowRecord.nickname+
-												'</div>'+
-												'<div>'+
-												'<h6 class="left  text-darken-1  BookTitle">'+'向TA借阅'+'&nbsp;&nbsp;&nbsp;</h6>'+
-												'<h6 class="left  text-darken-1 BookTitle"><b>《'+borrowRecord.book_name+'》</b></h6>'+
-												'</div>'+
-											'</div>'+
-											'<div class="status center">'+
-												'<span  style="color:#807e7e"><br>'+sstatus+'</span>'+
-											'</div>'+
+			},
+			onFailure: function(apiError){
+				Materialize.toast(apiError.getErrorMessage(), 4000)
+			}
+		
+		});
+	}
+	
+	function insertBorrowRecord(record){
+		var status;
+		if(record.status==1){
+			status='<span  style="color:#807e7e"><br>等待处理</span>';
+		}else if(record.status==2){
+			status='<span  style="color:#807e7e"><br>已同意</span>';
+		}else if(record.status==3){
+			status='<span  style="color:#807e7e"><br>被拒绝</span>';
+		}else if(record.status==4){
+			status='<span  style="color:#807e7e"><br>已归还</span>';
+		}else{
+			status="";
+		}	
+		
+		var stime= record.borrow_time.split(" ");
+		stime[1] = stime[1].split(":");
+				
+		$("#Borrow").append(
+			'<li id="BorrowIn1" class="row">'+
+				'<time class="cbp_tmtime col m2 l3 offset-l6 hide-on-small-only" datetime='+record.borrow_time+'>'+
+					'<span>'+stime[0]+'</span>'+
+					'<span>'+stime[1][0]+":"+stime[1][1]+'</span>'+
+				'</time>'+
+				'<div class="cbp_tmicon cbp_tmicon-phone hide-on-med-and-down"></div>'+
+					'<div class="col s12 m10 l5 offset-l0">'+
+						'<div class="card white darken-1 hoverable borrowcard">'+
+							'<div class="BookDetail">'+
+								'<div class="card-content black-text" style="height: 100%">'+
+									'<div style="overflow: auto;">'+
+										'<div class="chips">'+
+											'<img src="http://115.28.135.76/images/users/avatars/'+record.loan_uid+'.png" alt="Contact Person">'+
+											record.nickname+
+										'</div>'+
+										'<div>'+
+										'<h6 class="left  text-darken-1  BookTitle">'+'向TA借阅'+'&nbsp;&nbsp;&nbsp;</h6>'+
+										'<h6 class="left  text-darken-1 BookTitle"><b>《'+record.book_name+'》</b></h6>'+
 										'</div>'+
 									'</div>'+
+									'<div class="status center">'+
+										status+
+									'</div>'+
 								'</div>'+
+							'</div>'+
 						'</div>'+
-					'</li>'
-                       
-				)
-			});
+				'</div>'+
+			'</li>'
+			   
+		);
+	}
+	
+	function insertLoanRecord(record){
+		var status;
+		if(record.status==1){
+			status='<a class="waves-effect waves-dark btn-flat agree" onClick="acceptRecored('+record.brid+',true)">借出</a>'+
+            	   '<a class="waves-effect waves-dark btn-flat refuse" onClick="acceptRecored('+record.brid+',false)">拒绝</a>';
+		}else if(record.status==2){
+			status='<span  style="color:#807e7e"><br>已同意</span>';
+		}else if(record.status==3){
+			status='<span  style="color:#807e7e"><br>已拒绝</span>';;
+		}else if(record.staus==4){
+			status='<span  style="color:#807e7e"><br>已归还</span>';;
+		}else{
+			status="";
 		}
 		
-	});
-	
-	PLServerAPI.getLoanedBookRecordList(0, {
-		onSuccess: function(loanedRecords){
-			
-			var sstatus;
-			$.each(loanedRecords, function(i, loanRecord){
-				if(loanedRecord.status==1){
-					sstatus="等待处理";
-				}else if(borrowRecord.status==2){
-					sstatus="已同意";
-				}else if(loanedRecord.status==3){
-					sstatus="被拒绝";
-				}else if(loanedRecord.staus==4){
-					sstatus="已归还";
-				}else{
-					sstatus="";
-				}
-			
+		var stime= record.borrow_time.split(" ");
+		stime[1] = stime[1].split(":");
 				
 				$("#Borrow").append(
 					'<li id="BorrowIn1" class="row">'+
-						'<time class="cbp_tmtime col m2 l3 offset-l3 hide-on-small-only" datetime='+loanedRecord.borrow_time+'>'+
+						'<time class="cbp_tmtime col m2 l3 offset-l3 hide-on-small-only" datetime='+record.borrow_time+'>'+
 							'<span>'+stime[0]+'</span>'+
-							'<span>'+stime[1]+'</span>'+
+							'<span>'+stime[1][0]+":"+stime[1][1]+'</span>'+
 						'</time>'+
 						'<div class="cbp_tmicon cbp_tmicon-phone hide-on-med-and-down"></div>'+
 							'<div class="col s12 m10 l5 offset-l7">'+
@@ -128,16 +139,16 @@ $(document).ready(function(e) {
 										'<div class="card-content black-text" style="height: 100%">'+
 											'<div style="overflow: auto;">'+
 												'<div class="chips">'+
-													'<img src="http://115.28.135.76/images/users/avatars/'+loanedRecord.borrow_uid+'.png" alt="Contact Person">'+
-													loanedRecord.nickname+
+													'<img src="http://115.28.135.76/images/users/avatars/'+record.borrow_uid+'.png" alt="Contact Person">'+
+													record.nickname+
 												'</div>'+
 												'<div>'+
-												'<h6 class="left  text-darken-1  BookTitle">'+'向TA借阅'+'&nbsp;&nbsp;&nbsp;</h6>'+
-												'<h6 class="left  text-darken-1 BookTitle"><b>《'+loanedRecord.book_name+'》</b></h6>'+
+												'<h6 class="left  text-darken-1  BookTitle">'+'请求借阅'+'&nbsp;&nbsp;&nbsp;</h6>'+
+												'<h6 class="left  text-darken-1 BookTitle"><b>《'+record.book_name+'》</b></h6>'+
 												'</div>'+
 											'</div>'+
 											'<div class="status center">'+
-												'<span  style="color:#807e7e"><br>'+sstatus+'</span>'+
+												status+
 											'</div>'+
 										'</div>'+
 									'</div>'+
@@ -145,10 +156,19 @@ $(document).ready(function(e) {
 						'</div>'+
 					'</li>'
                        
-				)
-			});
-		}
-		
-	});
+				);
+	};
 	
-});
+	
+	
+	
+	function acceptRecored(brid, accept){
+			PLServerAPI.acceptBorrowRecord(brid,accept,{
+				onSuccess:function(){
+					getRecords();
+				},
+				onFailure:function(){
+					Materialize.toast(apiError.getErrorMessage(), 4000);
+				}
+			});
+	}
